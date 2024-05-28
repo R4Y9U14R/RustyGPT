4f6f0baa35441b5bc39db40168a748f6d64d0ff9
Ray Li <seriouscoder123@gmail.com>
2024-05-28 11:22:48 +0800

    Add build script

---
+++ build.rs
@@ -0,0 +1,26 @@
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+use std::process::Command;
+
+fn main() {
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let out_dir = PathBuf::from(&manifest_dir).join("libs");
+    let cuda_file = "src/kernels/dense.cu";
+
+    fs::create_dir_all(&out_dir).expect("Failed to create directory");
+
+    let status = Command::new("nvcc")
+        .args(&["-shared", "-o", &format!("{}/libdense.so", out_dir.display()), "-Xcompiler", "-fPIC", cuda_file])
+        .status()
+        .expect("Failed to compile CUDA file.");
+
+    if !status.success() {
+        panic!("nvcc failed to compile the CUDA file");
+    }
+
+    println!("cargo:rustc-link-search=native={}", out_dir.display());
+    println!("cargo:rustc-link-lib=dylib=dense");
+    println!("cargo:rerun-if-changed={}", cuda_file);
+    println!("cargo:rustc-env=LD_LIBRARY_PATH={}", out_dir.display());
+}
