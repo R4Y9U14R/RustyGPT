42cdeaeb8a03a099d30f730f82a41c02a7901040
Guangyao Ray Li <guangyaor.li2010@gmail.com>
2024-06-30 15:53:35 +0800

    Ensure directory exists

--- src/conversion_scripts/convert_to_bin.py
+++ src/conversion_scripts/convert_to_bin.py
@@ -9,7 +9,8 @@ def save_to_bin(path: str):
         print(f"Layer: {key}")
         print(f"Shape: {tuple(value.shape)}")
 
-        value.cpu().numpy().tofile(os.path.join("src/weights/processed", f"{key}-{(tuple(value.shape))}.bin"), dtype=np.float32)
+        value.cpu().numpy().tofile(os.path.join("src/weights/processed", f"{key}-{(tuple(value.shape))}.bin"))
 
 if __name__ == "__main__":
+    os.makedirs("src/weights/raw", exist_ok=True)
     save_to_bin(os.path.join("src/weights/raw", "mlp_model.pth"))
